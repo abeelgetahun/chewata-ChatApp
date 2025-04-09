92faf310812c171259b1f215758636081b05310a
Abel <abelgetahun66@gmail.com>
2025-04-09 15:06:14 +0300

    change the app structure

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:chewata/screen/onboarding.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Chewata App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: OnBoardingScreen()
+    );
+  }
+}
+
