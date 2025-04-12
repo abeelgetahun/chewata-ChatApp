65dfd1fb9810678ebd9ae1919ae96129f6bff1ae
Abel <abelgetahun66@gmail.com>
2025-04-12 11:56:20 +0300

    update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Chewata App',
+      title: 'Chewata chat',
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
