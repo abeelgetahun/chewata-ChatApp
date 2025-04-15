8a8c3b5e95e2f267105bb0eb6a84b43ad6896555
Abel <abelgetahun66@gmail.com>
2025-04-16 00:14:15 +0300

    Implement dynamic background image selection based on theme mode in AuthScreen

--- lib/screen/auth/auth_screen.dart
+++ lib/screen/auth/auth_screen.dart
@@ -18,6 +18,14 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
+    // Determine the current theme mode (light or dark)
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
+    // Select the appropriate background image based on the theme
+    final backgroundImage = isDarkMode
+        ? "assets/images/auth_images/login_background_dark.jpg"
+        : "assets/images/auth_images/login_background.jpg";
+
     // Calculate dynamic height for the box
     double boxHeight;
     if (size.width < 600) {
@@ -31,11 +39,11 @@ class _AuthScreenState extends State<AuthScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          // Static background image
+          // Dynamic background image
           Container(
-            decoration: const BoxDecoration(
+            decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage("assets/images/auth_images/login_background.jpg"),
+                image: AssetImage(backgroundImage),
                 fit: BoxFit.cover, // Ensure the image covers the full screen
               ),
             ),
