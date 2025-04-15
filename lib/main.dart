dfc33a03fce8b6310537fae1fb550c3c486db4d2
Abel <abelgetahun66@gmail.com>
2025-04-15 21:22:50 +0300

    Add Firebase options and initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
-import 'package:chewata/app.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'firebase_options.dart';
+import 'package:chewata/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
-  //firebase
 }
