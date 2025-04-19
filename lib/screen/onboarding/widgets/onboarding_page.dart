c0be7e61caf35cfb0a4a2a5e76d54ad78eb1903d
Abel <abelgetahun66@gmail.com>
2025-04-19 19:38:11 +0300

    Add OnBoardingPage widget for onboarding screen with image and text display

---
+++ lib/screen/onboarding/widgets/onboarding_page.dart
@@ -0,0 +1,56 @@
+import 'package:chewata/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({
+    super.key, 
+    required this.image, 
+    required this.title, 
+    required this.subTitle,
+  });
+
+  final String image, title, subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    // Get dimensions based on the current context
+    final Size size = MediaQuery.of(context).size;
+    final double screenWidth = size.width;
+    final double screenHeight = size.height;
+    
+    return Padding(
+      padding: const EdgeInsets.all(TSize.defaultSpace),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: screenHeight * 0.05), // Offset for the skip button
+          Lottie.asset(
+            image,
+            width: screenWidth * 0.8,
+            height: screenHeight * 0.5,
+            fit: BoxFit.contain,
+            frameRate: FrameRate.max,
+          ),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+              fontFamily: 'WinkySans',
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: TSize.spaceBtwItems),
+          Text(
+            subTitle,
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              color: Theme.of(context).colorScheme.onSurfaceVariant,
+              fontFamily: 'WinkySans'
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
