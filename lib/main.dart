243f162d7310621c82a7da642f6502b880ef1f06
Abel <abelgetahun66@gmail.com>
2025-04-28 00:06:12 +0300

    Initialize theme controller in main function

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 import 'package:chewata/app.dart';
+import 'package:chewata/controller/theme_controller.dart';
+import 'package:get/get.dart';
 
 void main() async {
   // Ensure Flutter bindings are initialized in the same zone
@@ -17,6 +19,9 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Initialize theme controller
+  await Get.putAsync(() async => await ThemeController().init());
 
   // Set up global error handling
   runZonedGuarded(() {
