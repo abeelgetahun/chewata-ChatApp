d0b5dfc1747f90266ca3cb4bd3b83937f53ef9d9
Abel <abelgetahun66@gmail.com>
2025-04-27 19:24:23 +0300

    Add AccountScreen widget to display account information

---
+++ lib/screen/account_screen.dart
@@ -0,0 +1,16 @@
+// lib/screen/account_screen.dart
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Account Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
