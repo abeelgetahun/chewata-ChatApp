2f34c43ab3c57b9a7f85cddd0161e1e0770907c2
Abel <abelgetahun66@gmail.com>
2025-04-28 00:06:40 +0300

    ThemeController to manage theme mode initialization and preferences

--- lib/controller/theme_controller.dart
+++ lib/controller/theme_controller.dart
@@ -1,25 +1,67 @@
+// lib/controller/theme_controller.dart
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeController extends GetxController {
-  // Reactive variable to store the current theme mode
-  var themeMode = ThemeMode.system.obs;
-
-  // Set the theme mode
-  void setThemeMode(ThemeMode mode) {
-    themeMode.value = mode;
-    Get.changeThemeMode(mode);
+  static ThemeController get instance => Get.find();
+  
+  final _themeMode = Rx<ThemeMode>(ThemeMode.system);
+  late SharedPreferences _prefs;
+  
+  Future<ThemeController> init() async {
+    _prefs = await SharedPreferences.getInstance();
+    loadThemeMode();
+    return this;
   }
-
-  // Get the current theme icon based on the theme mode
-  IconData getThemeIcon() {
-    switch (themeMode.value) {
-      case ThemeMode.dark:
-        return Icons.dark_mode;
+  
+  void loadThemeMode() {
+    final savedThemeMode = _prefs.getString('themeMode');
+    
+    if (savedThemeMode != null) {
+      switch (savedThemeMode) {
+        case 'light':
+          _themeMode.value = ThemeMode.light;
+          break;
+        case 'dark':
+          _themeMode.value = ThemeMode.dark;
+          break;
+        default:
+          _themeMode.value = ThemeMode.system;
+      }
+    }
+  }
+  
+  ThemeMode getThemeMode() {
+    return _themeMode.value;
+  }
+  
+  Future<void> setThemeMode(ThemeMode mode) async {
+    _themeMode.value = mode;
+    
+    // Save preference
+    await _prefs.setString('themeMode', mode.toString().split('.').last);
+    
+    // Update the app theme
+    Get.changeThemeMode(mode);
+    update();
+  }
+  
+  bool get isDarkMode {
+    if (_themeMode.value == ThemeMode.system) {
+      return WidgetsBinding.instance.platformDispatcher.platformBrightness == Brightness.dark;
+    }
+    return _themeMode.value == ThemeMode.dark;
+  }
+  
+  String get currentThemeText {
+    switch (_themeMode.value) {
       case ThemeMode.light:
-        return Icons.light_mode;
-      default:
-        return Icons.brightness_auto;
+        return 'Light';
+      case ThemeMode.dark:
+        return 'Dark';
+      case ThemeMode.system:
+        return 'System';
     }
   }
 }
