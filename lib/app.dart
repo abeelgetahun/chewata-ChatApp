f19a1c3496729c9c60541b10f8e1b92b2d268cdf
Abel <abelgetahun66@gmail.com>
2025-05-01 22:27:44 +0300

    Register ChatService and ensure ChatController is initialized in the app

--- lib/app.dart
+++ lib/app.dart
@@ -6,6 +6,7 @@ import 'package:chewata/screen/chat/chat_screen.dart';
 import 'package:chewata/screen/home_screen.dart';
 import 'package:chewata/screen/onboarding/onboarding.dart';
 import 'package:chewata/services/auth_service.dart';
+import 'package:chewata/services/chat_service.dart';
 import 'package:chewata/utils/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -18,6 +19,8 @@ class App extends StatelessWidget {
     // Initialize services with Get
     Get.put(AuthService(), permanent: true);
     Get.put(AuthController(), permanent: true);
+    Get.put(ChatService(), permanent: true);
+    
     
     // Get theme controller (already initialized in main.dart)
     final ThemeController themeController = Get.find<ThemeController>();
@@ -29,23 +32,23 @@ class App extends StatelessWidget {
       darkTheme: TAppTheme.darkTheme,
       initialRoute: '/',
       getPages: [
-  GetPage(name: '/', page: () => _determineInitialScreen()),
-  GetPage(name: '/onboarding', page: () => const OnBoardingScreen()),
-  GetPage(name: '/auth', page: () => const AuthScreen()),
-  GetPage(name: '/home', page: () => const HomeScreen()),
-  GetPage(
-    name: '/chat/:chatId',
-    page: () {
-      final chatId = Get.parameters['chatId']!;
-      return ChatScreen(chatId: chatId);
-    },
-    binding: BindingsBuilder(() {
-      if (!Get.isRegistered<ChatController>()) {
-        Get.put(ChatController());
-      }
-    }),
-  ),
-],
+      GetPage(name: '/', page: () => _determineInitialScreen()),
+      GetPage(name: '/onboarding', page: () => const OnBoardingScreen()),
+      GetPage(name: '/auth', page: () => const AuthScreen()),
+      GetPage(name: '/home', page: () => const HomeScreen()),
+      GetPage(
+            name: '/chat/:chatId',
+            page: () {
+              final chatId = Get.parameters['chatId']!;
+              return ChatScreen(chatId: chatId);
+            },
+            binding: BindingsBuilder(() {
+              if (!Get.isRegistered<ChatController>()) {
+                Get.put(ChatController());
+              }
+            }),
+          ),
+    ],
     ));
   }
   
