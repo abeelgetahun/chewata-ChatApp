ed6ac3e72e67e8fca6fdb500169d6097418ebfab
Abrham Kifle Kebede <abrihott@gmail.com>
2025-05-12 08:27:26 +0300

    Update connect_screen.dart

--- connect_screen.dart
+++ connect_screen.dart
@@ -1,1 +1,16 @@
 
+import 'package:flutter/material.dart';
+
+class ConnectScreen extends StatelessWidget {
+  const ConnectScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Connect Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
