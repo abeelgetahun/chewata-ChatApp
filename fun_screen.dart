c162262588f720dc0cad1e65a73fc4a8959ed2fa
Abrham Kifle Kebede <abrihott@gmail.com>
2025-05-12 08:28:32 +0300

    Update fun_screen.dart

--- fun_screen.dart
+++ fun_screen.dart
@@ -1,1 +1,16 @@
 
+import 'package:flutter/material.dart';
+
+class FunScreen extends StatelessWidget {
+  const FunScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Fun Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
